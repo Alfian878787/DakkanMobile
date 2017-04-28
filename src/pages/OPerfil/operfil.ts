4288fb57969a9c8837d361762ecf7e8d00a4d1b6
Alberto Antoñanzas <alberto.antonanzas@aaaida.com>
2017-04-28 16:29:45 +0200

    reviewholder

--- src/pages/OPerfil/operfil.ts
+++ src/pages/OPerfil/operfil.ts
@@ -16,12 +16,57 @@ import 'rxjs/add/operator/map';
 export class OPerfilPage {
 adv:any;
 date:any;
+reviews: any;
+
 
   constructor(public navParams: NavParams,public http: Http,public storage:Storage,public navCtrl: NavController, private camera: Camera, public toastCtrl: ToastController, public actionSheetCtrl: ActionSheetController, public loadingCtrl: LoadingController) {
     this.adv = navParams.get('adv');
-    console.log(this.adv)
     let timestamp = this.adv.owner.toString().substring(0,8);
     this.date = new Date( parseInt( timestamp, 16 ) * 1000 ).toLocaleDateString();
+    let data=
+      [{title : "ey",
+      description : "ey",
+      rating: 96},{
+      title:"hola",
+        description:"buena",
+        rating:20
+      },{
+        title:"hola",
+        description:"buena",
+        rating:20
+      },{
+        title:"hola",
+        description:"buena",
+        rating:20
+      },{
+        title:"hola",
+        description:"buena",
+        rating:20
+      },{
+        title:"hola",
+        description:"buena",
+        rating:20
+      }];
+
+    this.reviews=data;
+  }
+
+  getReviews(){
+    if (this.reviews) {
+      return Promise.resolve(this.reviews);
+    }
+
+    return new Promise(resolve => {
+
+     /* this.http.get('http://localhost:8080/api/reviews')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.reviews = data;
+        });*/
+
+
+    });
+
   }
 
 }
